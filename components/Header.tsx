1432eb71e371883d4f1b3391b0db81c60df2c2d6
Andrew Tate <whatcolorisyourbugatti@andrew.tate>
2022-12-05 17:42:32 +0100

    sui

--- components/Header.tsx
+++ components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
         </ul>
 
         <button className="border border-cg-blue text-foreground/60 h-8 rounded px-6 hover:bg-cg-blue transition-colors duration-300 font-bold">
-          Login
+          Renamed this btn to try out commits
         </button>
       </div>
     </nav>
