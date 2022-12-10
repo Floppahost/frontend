3e6fb67289c42eed1ea73e023ad38605e0445792
threedflow <yes.pes32@gmail.com>
2022-12-10 11:23:43 +0100

    several changes to main page

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -1,6 +1,27 @@
+<script>
+    import { ZapIcon } from "svelte-feather-icons";
+</script>
+
 <div class="flex h-screen main bg-center bg-no-repeat bg-contain">
     <div class="m-auto">
-        <img src="/logo.jpg" class="mx-auto my-auto w-96" alt="logo" />
-        <p class="text-xl font-bold">floppa.host</p>
+        <img
+            src="/logo.gif"
+            class="drop-shadow-md mx-auto my-auto w-96"
+            alt="logo"
+        />
+        <div class="grid grid-cols-3 gap-2 justify-items-center">
+            <div class="p-3 flex">
+                <ZapIcon class="my-auto" />
+                <p class="pl-2 font-bold my-auto text-2xl">Fast</p>
+            </div>
+            <div class="p-3 flex">
+                <ZapIcon class="my-auto" />
+                <p class="pl-2 font-bold my-auto text-2xl">Fast</p>
+            </div>
+            <div class="p-3 flex">
+                <ZapIcon class="my-auto" />
+                <p class="pl-2 font-bold my-auto text-2xl">Fast</p>
+            </div>
+        </div>
     </div>
 </div>
