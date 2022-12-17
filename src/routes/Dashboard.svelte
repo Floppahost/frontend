ad2df2da2032225cef7b277775dc50c09e7b0d70
barius <notijjj@gmail.com>
2022-12-17 12:57:33 +0200

    dashboard code

--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -5,8 +5,8 @@
 <div class="flex min-h-screen main bg-center bg-no-repeat bg-contain">
     <div class="m-auto">
         <div class="bg-cyan-400 rounded-lg">
-        <div class="font-mono text-left w-1/2 mx-auto">
-                <UserIcon size="small" />
+        <div class="font-mono px-8 w-max mx-auto">
+                <UserIcon class="mx-auto" size="70" />
                 <p class="font-bold">
                 Welcome, barius
                 </p>
