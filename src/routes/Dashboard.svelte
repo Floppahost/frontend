f419cd8e25c392febee2dc4936a2df9bd07e0db3
barius <notijjj@gmail.com>
2022-12-17 12:43:28 +0200

    changes

--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -4,9 +4,13 @@
 
 <div class="flex min-h-screen main bg-center bg-no-repeat bg-contain">
     <div class="m-auto">
+        <div class="bg-cyan-400 rounded-lg">
         <div class="font-mono text-left w-1/2 mx-auto">
                 <UserIcon size="small" />
+                <p class="font-bold">
                 Welcome, barius
+                </p>
             </div>
         </div>
     </div>
+</div>
