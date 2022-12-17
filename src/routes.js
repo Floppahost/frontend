4e37db68d7f3ffa58d64ec067742d8ae086ed914
barius <notijjj@gmail.com>
2022-12-17 12:33:59 +0200

    dashboard route + code

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Index from "./routes/Index.svelte";
 import Login from "./routes/Login.svelte";
+import Dashboard from "./routes/Dashboard.svelte";
 
 export const routes = {
     '/': Index,
-    '/login': Login
+    '/login': Login,
+    '/dashboard': Dashboard
 }
--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -1,4 +1,12 @@
 <script>
-    import { HelpCircleIcon } from "svelte-feather-icons";
-    import { blur } from "svelte/transition";
+    import { UserIcon } from "svelte-feather-icons";
 </script>
+
+<div class="flex min-h-screen main bg-center bg-no-repeat bg-contain">
+    <div class="m-auto">
+        <div class="font-mono text-left w-1/2 mx-auto">
+                <UserIcon size="small" />
+                Welcome, barius
+            </div>
+        </div>
+    </div>
