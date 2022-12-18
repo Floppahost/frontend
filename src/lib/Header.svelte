14187264c9c3ebbe4c3a9e9065f964f69466c055
barius <notijjj@gmail.com>
2022-12-18 15:52:05 +0200

    header don3 what color is your bugatti

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,9 @@
-<div class="grid">
+<div class="grid grid-cols-2 w-max mx-auto">
     <div class="absolute flex h-auto">
-        <p>hi!!!!</p>
+        <a class="text-sm rounded border border-cyan-400" href="#/">Index</a>
+        <a class="text-sm rounded border border-cyan-400" href="#/register">Register</a>
+        <a class="text-sm rounded border border-cyan-400" href="#/login">Login</a>
+        <a class="text-sm rounded border border-cyan-400" href="https://discord.gg/MEeam8nZRp">Discord</a>
     </div>
 </div>
 <slot />
