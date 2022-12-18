0556882b401b2d7137ece9af57cbbde788fc309b
barius <notijjj@gmail.com>
2022-12-18 14:48:26 +0200

    header

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   import Router from "svelte-spa-router";
   import { routes } from "./routes";
+  import Header from "./lib/Header.svelte"
 </script>
 
 <div class="text-white font-default">
   <Router {routes} />
 </div>
+
+<Header>x fix onle</Header>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<div class="grid">
+    <div class="flex min-h-screen">
+        <p>
+            hi!!!!
+        </p>
+    </div>
+</div>
