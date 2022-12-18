fd1dc0eb59b013787408228415b2c51dfc3b4433
threedflow <yes.pes32@gmail.com>
2022-12-18 13:32:49 +0100

    login

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,40 +1,28 @@
 <script>
-    import { HelpCircleIcon } from "svelte-feather-icons";
     import { blur } from "svelte/transition";
 </script>
 
 <div class="flex h-screen main bg-center bg-no-repeat bg-contain">
-    <div class="m-auto">
+    <div class="m-auto p-4">
         <img
             src="/logo.gif"
-            class="drop-shadow-md mx-auto my-auto w-96"
+            class="drop-shadow-md w-48 mx-auto"
             alt="logo"
             in:blur
         />
-        <div class="text-left sm:w-1/2 w-80 mx-auto">
-            <div class="flex">
-                <p class="text-3xl font-bold mx-auto">Log in</p>
-            </div>
-            <div class="flex">
-                <div class="w-12 h-auto mx-auto">
-                    <div class="w-px h-full bg-white mx-auto" />
-                </div>
-                <form method="post" class="space-y-2 my-2">
-                    <input
-                        class="w-auto outline-none border border-white bg-transparent px-2 py-px text-sm"
-                        placeholder="Name"
-                    />
-                    <input
-                        class="w-full outline-none border border-white bg-transparent px-2 py-px text-sm"
-                        placeholder="Password"
-                    />
-                </form>
-            </div>
-            <a
-                href="#/login"
-                class="w-max text-sm rounded-full border border-cyan-400 py-px px-4 m-2"
-                >Login</a
+        <form method="post" class="grid grid-cols-1 space-y-1">
+            <input
+                placeholder="Name"
+                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+            />
+            <input
+                placeholder="Password"
+                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+            />
+            <button
+                class="text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-bold py-1"
+                >Log in</button
             >
-        </div>
+        </form>
     </div>
 </div>
