76c7ae2dc4a6d8b5ee2bc8af5cf76a0cbcb7d2d4
barius <notijjj@gmail.com>
2022-12-18 15:24:43 +0200

    fiox

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,12 @@
   import Header from "./lib/Header.svelte";
 </script>
 
+
 <div class="text-white font-default">
-  <Header>
-    <Router {routes} />
-  </Header>
+  <slot>
+    <Router {routes}>
+  <Header>hi</Header>
+</Router>
+</slot>
 </div>
+
