08fa1a949089450a0de56fed3f63c3f3415c78c9
barius <notijjj@gmail.com>
2022-12-18 16:02:34 +0200

    header finished will be reworked

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,10 @@
-<div class="grid grid-cols-2 w-max mx-auto">
-    <div class="absolute flex h-auto">
+<div class="w-max mx-auto">
+    <div class="flex h-auto space-x-3">
         <a class="text-sm rounded border border-cyan-400" href="#/">Index</a>
         <a class="text-sm rounded border border-cyan-400" href="#/register">Register</a>
         <a class="text-sm rounded border border-cyan-400" href="#/login">Login</a>
         <a class="text-sm rounded border border-cyan-400" href="https://discord.gg/MEeam8nZRp">Discord</a>
+        <a class="text-sm rounded border border-cyan-400" href="#/logout">Log out</a>
     </div>
 </div>
 <slot />
