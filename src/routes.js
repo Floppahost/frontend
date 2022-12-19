d1e57839448ffe446fd95ac757921f3f0c1e156c
barius <notijjj@gmail.com>
2022-12-19 19:26:29 +0200

    route fix im idiot пенис член

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import Index from "./routes/Index.svelte";
 import Login from "./routes/Login.svelte";
 import Dashboard from "./routes/Dashboard.svelte";
-import NotFound from "./routes/NotFound.svelte";
 import Gallery from "./routes/Gallery.svelte";
+import NotFound from "./routes/NotFound.svelte";
+
 
 export const routes = {
     '/': Index,
     '/login': Login,
     '/dashboard': Dashboard,
-    '*': NotFound,
-    '/gallery': Gallery
+    '/gallery': Gallery,
+    '*': NotFound
 }
