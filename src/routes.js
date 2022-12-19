12312169a48da36c2c9a4da1d1ab935ebe052f58
barius <notijjj@gmail.com>
2022-12-19 18:04:00 +0200

    fix?

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Index from "./routes/Index.svelte";
 import Login from "./routes/Login.svelte";
 import Dashboard from "./routes/Dashboard.svelte";
 import NotFound from "./routes/NotFound.svelte";
-import Gallery from "./routes/Gallery.svelte"
+import Gallery from "./routes/Gallery.svelte";
 
 export const routes = {
     '/': Index,
---
+++ src/routes/Gallery.svelte
@@ -0,0 +1,5 @@
+<div class="flex min-h-screen rounded-lg">
+ <div class="m-auto sm:w-1/3 w-full">
+    <p>image</p>
+ </div>
+</div>
