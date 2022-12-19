f87eab93a22efb4ac7ef692064aca83314c4cec3
barius <notijjj@gmail.com>
2022-12-19 20:03:41 +0200

    gallery

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,5 +1,7 @@
-<div class="flex min-h-screen rounded-lg">
- <div class="m-auto sm:w-1/3 w-full">
+<div class="grid w-max min-h-screen m-auto">
+    <div class="m-auto rounded border border-cyan-400 text-sm "><p><b>barius's</b> images</p></div>
+    <div class="m-auto rounded-lg grid grid-cols-2 text-sm border space-x-1 w-max border-cyan-400">
+    <p>image</p>
     <p>image</p>
  </div>
 </div>
