4a17f2ad077772bc1bb23ee88078882b4bf37d57
barius <notijjj@gmail.com>
2022-12-19 12:48:54 +0200

    dahsbaord

--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -6,11 +6,30 @@
     <div class="m-auto">
         <div class="bg-cyan-400 rounded-lg">
         <div class="font-mono px-8 w-max mx-auto">
-                <UserIcon class="mx-auto" size="70" />
+                <UserIcon class="w-max mx-auto" size="65" />
                 <p class="font-bold">
-                Welcome, barius
+                Welcome, $username
+                </p>
+                <p class="w-max mx-auto">
+                UID: $uid
                 </p>
             </div>
         </div>
+            <div class="flex screen h-32 main">
+            <div class="m-auto bg-cyan-400 rounded-lg">
+                <div class="font-mono w-max mx-auto">
+                <a
+                    href="#/getconfig"
+                    class="text-sm rounded-full border border-cyan-400 py-px px-4 m-2 font-bold"
+                    >Download Config</a
+                >
+                <a
+                href="#/embed"
+                class="text-sm rounded-full border border-cyan-400 py-px px-4 m-2 font-bold"
+                >Embed Settings</a
+            >
+            </div>
+        </div>
+            </div>
+        </div>
     </div>
-</div>
