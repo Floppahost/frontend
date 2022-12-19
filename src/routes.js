db75155cd67205f9e5d7b62f80a214af65c57672
barius <notijjj@gmail.com>
2022-12-19 17:34:39 +0200

    gallery route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import Index from "./routes/Index.svelte";
 import Login from "./routes/Login.svelte";
 import Dashboard from "./routes/Dashboard.svelte";
 import NotFound from "./routes/NotFound.svelte";
+import Gallery from "./routes/Gallery.svelte"
 
 export const routes = {
     '/': Index,
     '/login': Login,
     '/dashboard': Dashboard,
-    '*': NotFound
+    '*': NotFound,
+    '/gallery': Gallery
 }
