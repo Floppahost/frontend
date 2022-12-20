623ce248ad7798b32457aa90fd55c39ab4f5ee83
barius <notijjj@gmail.com>
2022-12-21 01:55:15 +0200

    um code but bad code ...

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -3,15 +3,21 @@
         <p><b>barius's</b> images</p>
     </div>
     <div
-        class="m-auto rounded-none grid grid-cols-2 text-sm border mt-8 md:w-96 border-cyan-400"
+        class="indent-1 m-auto rounded-none grid grid-cols-2 text-sm border mt-8 md:w-96 border-cyan-400"
     >
         <!-- svelte-ignore a11y-missing-attribute -->
         <img
          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
          />
+         <p class="inline">
+            <b>Uploaded at</b>: NPC
+         </p>
         <!-- svelte-ignore a11y-missing-attribute -->
         <img
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
         />
+        <p class="inline">
+            <b>Uploaded at</b>: NPC
+        </p>
     </div>
 </div>
