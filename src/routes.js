7f98c56e9cfe0180edbd7f549dcb5ce660fa39b0
barius <notijjj@gmail.com>
2022-12-20 16:32:42 +0200

    register route + code

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Index from "./routes/Index.svelte";
 import Login from "./routes/Login.svelte";
+import Register from "./routes/Register.svelte";
 import Dashboard from "./routes/Dashboard.svelte";
 import Gallery from "./routes/Gallery.svelte";
 import NotFound from "./routes/NotFound.svelte";
@@ -8,6 +9,7 @@ import NotFound from "./routes/NotFound.svelte";
 export const routes = {
     '/': Index,
     '/login': Login,
+    '/register': Register,
     '/dashboard': Dashboard,
     '/gallery': Gallery,
     '*': NotFound
---
+++ src/routes/Register.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { blur } from "svelte/transition";
+</script>
+
+<div class="flex h-screen">
+    <div class="m-auto p-4">
+        <img
+            src="/logo.gif"
+            class="drop-shadow-md w-48 mx-auto"
+            alt="logo"
+            in:blur
+        />
+        <form method="post" class="grid grid-cols-1 space-y-1">
+            <input
+                placeholder="Name"
+                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+            />
+            <input
+                placeholder="Password"
+                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+            />
+            <input
+                placeholder="Invite Code"
+                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+            />
+            <button
+                class="text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-bold py-1"
+                >Register</button
+            >
+        </form>
+    </div>
+</div>
