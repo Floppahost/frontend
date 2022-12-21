b728e603a3941bfd63a0c019b167d67cb806b4df
barius <notijjj@gmail.com>
2022-12-21 19:52:30 +0200

    code

--- src/routes/Gallery.svelte
+++ src/routes/Gallery.svelte
@@ -1,23 +1,23 @@
 <div class="grid w-max min-h-screen m-auto">
-    <div class="m-auto rounded-none border border-cyan-400 text-sm ">
-        <p><b>barius's</b> images</p>
-    </div>
-    <div
-        class="indent-1 m-auto rounded-none grid grid-cols-2 text-sm border mt-8 md:w-96 border-cyan-400"
-    >
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <img
-         src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
-         />
-         <p class="inline">
-            <b>Uploaded at</b>: NPC
-         </p>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
-        />
-        <p class="inline">
-            <b>Uploaded at</b>: NPC
-        </p>
-    </div>
+  <div class="m-auto rounded-none border border-cyan-400 text-sm ">
+      <p><b>barius's</b> images</p>
+  </div>
+  <div
+      class="indent-1 m-auto rounded-none grid grid-cols-2 text-sm border mt-8 md:w-96 border-cyan-400"
+  >
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img
+       src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
+       />
+       <p class="inline">
+          <b>Uploaded at</b>: NPC
+       </p>
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1200px-Big_Floppa_and_Justin_2_%28cropped%29.jpg"
+      />
+      <p class="inline">
+          <b>Uploaded at</b>: NPC
+      </p>
+  </div>
 </div>
