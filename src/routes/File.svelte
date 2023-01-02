c1f8468ddb7fbbd285030b972906802346ca40b5
barius <notijjj@gmail.com>
2023-01-02 15:14:43 +0200

    :skull: 💀 💀

---
+++ src/routes/File.svelte
@@ -0,0 +1,23 @@
+<div class="flex h-screen">
+    <div class="m-auto sm:w-1/2">
+        <div class="grid sm:grid-cols-2 grid-cols-1 gap-4 w-full mx-auto">
+            <div class="backdrop-blur-lg bg-cyan-400/10 p-4 rounded-3xl h-auto">
+                <div class="flex space-x-2">
+                    <img
+                        src="https://i.imgur.com/QD4KnPZ.jpeg"
+                        alt="peterhook.png"
+                        class="w-24 rounded-lg"
+                    />
+                    <div class="my-auto">
+                        <p class="text-2xl font-bold">peterhook.png</p>
+                        <p class="text-1xl">File size: <b>1.32KB</b></p>
+                        <p>
+                            Uploaded by <b>barius</b>
+                        </p>
+                        <p>Uploaded at <b>19:01:06</b></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
