eca2af7658eade1d1dfbeff0362282c68d3c5edb
threedflow <yes.pes32@gmail.com>
2023-01-04 17:32:29 +0100

    added auth fallback route

---
+++ src/routes/AccessDenied.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { blur } from "svelte/transition";
+</script>
+
+<div class="flex h-screen">
+    <div class="m-auto p-4">
+        <p class="text-center text-3xl font-bold">Access denied</p>
+        <p class="text-center">You can't view this page.</p>
+    </div>
+</div>
