b2fce8c798a6471d9c94d9ffe4aee2e08ca5a30d
threedflow <yes.pes32@gmail.com>
2023-01-04 17:38:47 +0100

    added authentication validator

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,28 @@
   import Router from "svelte-spa-router";
   import { routes } from "./routes";
   import Header from "./lib/Header.svelte";
+  import { onMount } from "svelte";
+  import AccessDenied from "./routes/AccessDenied.svelte";
+
+  $: status = 401;
+
+  // auth validator
+  onMount(async () => {
+    let response = await fetch("/status", {
+      method: "post",
+      credentials: "include",
+    });
+
+    status = response.status;
+  });
 </script>
 
 <div class="text-white font-default mx-2">
   <Header>
-    <Router {routes} />
+    {#if status == 202}
+      <Router {routes} />
+    {:else}
+      <AccessDenied />
+    {/if}
   </Header>
 </div>
--- src/routes/AccessDenied.svelte
+++ src/routes/AccessDenied.svelte
@@ -2,7 +2,7 @@
     import { blur } from "svelte/transition";
 </script>
 
-<div class="flex h-screen">
+<div class="flex h-screen text-white">
     <div class="m-auto p-4">
         <p class="text-center text-3xl font-bold">Access denied</p>
         <p class="text-center">You can't view this page.</p>
