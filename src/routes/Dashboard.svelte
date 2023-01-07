cf39e3bb15caf944286a35b11b82dc01d36faa23
threedflow <yes.pes32@gmail.com>
2023-01-07 19:11:45 +0100

    register done, dashboard major improvements

--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -1,9 +1,18 @@
+<script>
+    import { UserIcon } from "svelte-feather-icons";
+</script>
+
 <div class="flex h-screen">
     <div class="m-auto sm:w-1/3 w-full">
         <div class="grid sm:grid-cols-2 grid-cols-1 gap-4 w-full mx-auto">
             <div class="backdrop-blur-lg bg-cyan-400/10 p-4 rounded-3xl">
                 <div class="flex">
-                    <p class="text-3xl font-bold">Account</p>
+                    <UserIcon size="24" class="my-auto" />
+                    <p class="pl-2 text-3xl font-bold">Account</p>
+                    <button
+                        class="text-sm rounded-full bg-[#191621] py-px px-4 ml-2 my-auto"
+                        >Reset password
+                    </button>
                 </div>
                 <div class="flex">
                     <div class="h-auto">
--- src/routes/Register.svelte
+++ src/routes/Register.svelte
@@ -1,5 +1,20 @@
 <script>
+    import { PlusIcon, AlertCircleIcon, CheckIcon } from "svelte-feather-icons";
     import { blur } from "svelte/transition";
+
+    let form = {};
+    let response = {};
+
+    async function register() {
+        let call = await fetch("/register", {
+            method: "post",
+            body: JSON.stringify(form),
+        });
+
+        response = {
+            status: call.status,
+        };
+    }
 </script>
 
 <div class="flex h-screen">
@@ -10,27 +25,56 @@
             alt="logo"
             in:blur
         />
-        <form method="post" class="grid grid-cols-1 space-y-1">
+        {#if response.status == 200}
+            <div class="flex w-80 bg-lime-500 text-sm px-2 py-1 my-auto">
+                <div class="flex mx-auto">
+                    <CheckIcon size="18" /><span class="pl-2"
+                        >Successfully created an account!</span
+                    >
+                </div>
+            </div>
+        {:else if response.status != null}
+            <div class="flex w-80 bg-red-500 text-sm px-2 py-1 my-auto">
+                <div class="flex mx-auto">
+                    <AlertCircleIcon size="18" /><span class="pl-2"
+                        >Invalid data</span
+                    >
+                </div>
+            </div>
+        {/if}
+        <form
+            method="post"
+            class="grid grid-cols-1 space-y-1 mt-2"
+            on:submit|preventDefault={register}
+        >
             <input
                 placeholder="Name"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.name}
             />
             <input
                 placeholder="Email"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.email}
             />
             <input
                 placeholder="Password"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.password}
             />
             <input
-                placeholder="Invite Code"
+                placeholder="Invitation code"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.invite}
             />
             <button
-                class="text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-bold py-1"
-                >Register</button
+                class="flex text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-semibold py-1 my-auto"
+                type="submit"
             >
+                <div class="flex mx-auto">
+                    <PlusIcon size="18" /><span class="pl-2">Register</span>
+                </div>
+            </button>
         </form>
     </div>
 </div>
