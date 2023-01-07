c7d3c5079d31f19f2aa3914bc8abf801b0fe6562
threedflow <yes.pes32@gmail.com>
2023-01-07 19:00:16 +0100

    added form logic (done)

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -1,6 +1,24 @@
 <script>
-    import { UnlockIcon } from "svelte-feather-icons";
+    import {
+        UnlockIcon,
+        AlertCircleIcon,
+        CheckIcon,
+    } from "svelte-feather-icons";
     import { blur } from "svelte/transition";
+
+    let form = {};
+    $: response = { status: null };
+
+    async function login() {
+        let call = await fetch("/login", {
+            method: "post",
+            body: JSON.stringify(form),
+        });
+
+        response = {
+            status: call.status,
+        };
+    }
 </script>
 
 <div class="flex h-screen">
@@ -11,17 +29,41 @@
             alt="logo"
             in:blur
         />
-        <form method="post" class="grid grid-cols-1 space-y-1">
+        {#if response.status == 200}
+            <div class="flex w-80 bg-lime-500 text-sm px-2 py-1 my-auto">
+                <div class="flex mx-auto">
+                    <CheckIcon size="18" /><span class="pl-2"
+                        >Successfully logged in!</span
+                    >
+                </div>
+            </div>
+        {:else if response.status != null}
+            <div class="flex w-80 bg-red-500 text-sm px-2 py-1 my-auto">
+                <div class="flex mx-auto">
+                    <AlertCircleIcon size="18" /><span class="pl-2"
+                        >Invalid data</span
+                    >
+                </div>
+            </div>
+        {/if}
+        <form
+            method="post"
+            class="grid grid-cols-1 space-y-1 mt-2"
+            on:submit|preventDefault={login}
+        >
             <input
                 placeholder="Name"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.name}
             />
             <input
                 placeholder="Password"
                 class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                bind:value={form.password}
             />
             <button
                 class="flex text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-semibold py-1 my-auto"
+                type="submit"
             >
                 <div class="flex mx-auto">
                     <UnlockIcon size="18" /><span class="pl-2">Log in</span>
