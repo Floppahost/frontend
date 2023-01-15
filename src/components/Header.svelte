6429747a0e4fea369d81f14528bce7d3b483d761
threedflow <yes.pes32@gmail.com>
2023-01-15 14:38:26 +0100

    bla bla bla

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,9 +2,9 @@
     import {push} from 'svelte-spa-router'
 
     let paths = [
-        'dashboard',
-        'settings',
-        'gallery'
+        {name: 'dashboard', path: "/dashboard"},
+        {name: 'settings', path: "/dashboard/settings"},
+        {name: 'gallery', path: "/dashboard/gallery"}
     ]
 
     let selected;
@@ -13,10 +13,10 @@
 <div class="flex justify-center items-center p-4 absolute m-auto left-0 right-0">
     <div class="flex space-x-2 w-96">
         <img src="/logo.png" alt="logo" class="w-12 mr-16">
-        <select bind:value={selected} class="bg-neutral-800 text-sm border border-neutral-700 px-2 w-full" on:change={() => push("/" + selected)}>
+        <select bind:value={selected} class="bg-neutral-800 text-sm border border-neutral-700 px-2 w-full" on:change={() => push(selected)}>
             {#each paths as path}
-                <option value={path}>
-                    {path}
+                <option value={path.path}>
+                    {path.name}
                 </option>
 		    {/each}
         </select>
