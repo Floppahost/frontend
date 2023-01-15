b595167ce7531e9e6054b70e48931b4a1d5fa9a4
threedflow <yes.pes32@gmail.com>
2023-01-15 14:28:57 +0100

    deges fakt

--- src/routes/Register.svelte
+++ src/routes/Register.svelte
@@ -1,63 +1,28 @@
 <script>
-    import { PlusIcon, AlertCircleIcon, CheckIcon } from "svelte-feather-icons";
-    import { blur } from "svelte/transition";
+    import Response from "../components/Response.svelte";
 
     let form = {};
     let response = {};
-    
-    let inviteOnly = true
-    onMount(async()=>{
-        const request = await fetch(`${domain}/status/invite`)
-        request.json().then((data)=> {
-            inviteOnly = data.inviteOnly
-        })
-    })
 
-    let error = ""
-    const register = async () => {
-        const request = await fetch(`${domain}/auth/register`, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
+    async function register() {
+        let call = await fetch("/register", {
+            method: "post",
             body: JSON.stringify(form),
         });
 
         response = {
             status: call.status,
+            message: "alo"
         };
-
-        if (response.status === 200) {
-            location.href = "#/dashboard"
-        }
     }
 </script>
 
 <div class="flex h-screen">
     <div class="m-auto p-4">
-        <img
-            src="/logo.gif"
-            class="drop-shadow-md w-48 mx-auto"
-            alt="logo"
-            in:blur
-        />
-        {#if response.status == 200}
-            <div class="flex w-80 bg-lime-500 text-sm px-2 py-1 my-auto">
-                <div class="flex mx-auto">
-                    <CheckIcon size="18" /><span class="pl-2"
-                        >Successfully created an account!</span
-                    >
-                </div>
-            </div>
-        {:else if response.status != null}
-            <div class="flex w-80 bg-red-500 text-sm px-2 py-1 my-auto">
-                <div class="flex mx-auto">
-                    <AlertCircleIcon size="18" /><span class="pl-2"
-                        >Invalid data</span
-                    >
-                </div>
-            </div>
-        {/if}
+        <div class="flex justify-between">
+            <p class="font-extrabold text-left text-3xl">Register</p>
+            <a href="#/" class="ease-out duration-200 hover:px-6 px-4 h-min py-px rounded-lg bg-neutral-800 text-xs font-black my-auto">BACK</a>
+        </div>
         <form
             method="post"
             class="grid grid-cols-1 mt-2"
@@ -67,7 +32,7 @@
             <input
                 type="text"
                 placeholder="Name"
-                class="text-white w-80 bg-transparent border border-cyan-400 outline-none text-sm px-2 py-1"
+                class="w-80 bg-neutral-800 outline-none text-sm px-2 py-px rounded-lg"
                 bind:value={form.name}
                 required
             />
@@ -85,7 +50,6 @@
                 bind:value={form.password}
                 required
             />
-            {#if inviteOnly}
             <input
                 type="text"
                 placeholder="Invitation code"
@@ -93,13 +57,8 @@
                 bind:value={form.invite}
                 required
             />
-            <button
-                class="flex text-white w-80 bg-cyan-400 outline-none text-sm px-2 font-semibold py-1 my-auto"
-                type="submit"
-            >
-                <div class="flex mx-auto">
-                    <PlusIcon size="18" /><span class="pl-2">Register</span>
-                </div>
+            <button type="submit" class="mt-4 px-4 py-px font-bold rounded-lg bg-cyan-500 shadow-lg shadow-cyan-500/60">
+                Register
             </button>
         </form>
     </div>
